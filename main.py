c5c59dea63a72cc57acb9f480b598f6a2f578007
Juan Martin Lequerica <juan.martin.lequerica@jmlequerica-ch--ul>
2020-06-04 08:13:11 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+def sum(a,b):
+    return int(a)+int(b)
+
+
+print("insert numer 1")
+number1 = input()
+print("insert number 2")
+number2 = input()
+
+result = sum(number1,number2)
+print(result)
