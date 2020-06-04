8170e5e152ca72608bde2760e0b1249675d55ffa
Juan Martin Lequerica <juan.martin.lequerica@jmlequerica-ch--ul>
2020-06-04 08:15:50 -0500

    now it multiplies and we can sum floats

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-
 def sum(a,b):
+    return float(a)+float(b)
+def mult(a,b):
     return int(a)+int(b)
 
 
@@ -9,4 +10,6 @@ print("insert number 2")
 number2 = input()
 
 result = sum(number1,number2)
+result2 = mult(number1,number2)
 print(result)
+print(result2)
